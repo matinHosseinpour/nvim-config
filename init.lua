5d9ed0fa99806014f999c78d41f6417f04c0c227
matin <matin@matins-MacBook-Air.local>
2025-04-05 01:42:13 +0330

    add window splitter

--- init.lua
+++ init.lua
@@ -5,6 +5,12 @@ vim.opt.number = true
 vim.opt.relativenumber = false
 vim.opt.clipboard = "unnamedplus"
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Vertical Split" })
+vim.keymap.set("n", "<leader>sh", ":split<CR>", { desc = "Horizontal Split" })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom split" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top split" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
 
 -- Lazy.nvim bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
